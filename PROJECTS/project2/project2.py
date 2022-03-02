11edd92c0a30fafc17148921030b0238980cf805
Pranav <pranavcosmos4@gmail.com>
2022-03-02 12:34:00 +0530

    project 2 updated

--- PROJECTS/project2/project2.py
+++ PROJECTS/project2/project2.py
@@ -6,14 +6,19 @@ LOGIC:
 - Scan for rectangle sheet with Contour detection and identify the four corners
 - Warp the document
 
+CONS:
+- ONLY WORKS ON CONTRASTING BACKGROUNDS.
+- MIGHT DETECT WRONG PART OF PAPER
+
 """
 
 import cv2
 import numpy as np
+from sqlalchemy import true
 
 height = 750
 width = 500
-img = cv2.imread("images/image2.jpg") # using image coz my webcam sucks :(
+img = cv2.imread("images/image1.jpg") # using image coz my webcam sucks :(
 img = cv2.resize(img, (width, height))    # change (width, height) accordingly with different images
 
 # initializing warped img
@@ -22,13 +27,14 @@ warped = np.zeros_like(img)
 # detecting edges
 blur = cv2.GaussianBlur(img, (5,5), 0)
 canny = cv2.Canny(blur, 200, 200)
+dilate = cv2.dilate(canny, (5,5), iterations=2)
 
 # detecting shapes and isolating a rectangle
-contour, hierarchy = cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+contour, hierarchy = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 for cnt in contour:
-    print(cv2.contourArea(cnt))
-    if cv2.contourArea(cnt) > 5000:
+    if cv2.contourArea(cnt) > 500:
         corners = cv2.approxPolyDP(cnt, 50, True)   # corner points array shape (4,1,2)
+
         if len(corners) == 4:
             corners = np.reshape(corners, (4,2))
 
@@ -60,16 +66,17 @@ for cnt in contour:
                         if x > y:
                             right_of_origin = [x, y]
                 
-            cropPoints = np.array([origin, right_of_origin, diagonal, down_of_origin], np.float32)
-            newPoints = np.array([[0,0], [width, 0], [width, height], [0, height]], np.float32)
+            cropPoints = np.array([origin, right_of_origin, diagonal, down_of_origin], np.float32)  # crop points
+            newPoints = np.array([[0,0], [width, 0], [width, height], [0, height]], np.float32)     # destination points
 
-            matrix = cv2.getPerspectiveTransform(cropPoints, newPoints)
-            warped = cv2.warpPerspective(img, matrix, (width, height))
+            matrix = cv2.getPerspectiveTransform(cropPoints, newPoints)                             # perspective matrix
+            warped = cv2.warpPerspective(img, matrix, (width, height))                              # warped image
+
+            rectangle = cv2.polylines(img, [corners], True, (0,0,255), 2)                           # rectangle on image
             
             
 
 
 cv2.imshow("warped", warped)
-cv2.imshow("canny", canny)
 cv2.imshow("image", img)
 cv2.waitKey(0)
